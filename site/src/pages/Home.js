1318e85daaf8ed6667a74edb093df8d5dc96bb9b
2nthony <ant.hu@outlook.com>
2022-07-12 17:44:53 +0800

    compose home page

--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { Container } from "@osn/common-ui";
 import Background from "components/Background";
 import { MOBILE_SIZE } from "@osn/constants";
+import { h3_36_bold, p_20_semibold } from "@osn/common-ui/es/styles/textStyles";
+import ChildBountyList from "components/Bounty/ChildBountyList";
 
 const Wrapper = styled.div`
   position: relative;
@@ -19,12 +21,30 @@ const ContentWrapper = styled.div`
   }
 `;
 
+const Title = styled.h3`
+  ${h3_36_bold}
+`;
+
+const SubTitle = styled.p`
+  ${p_20_semibold}
+`;
+
 export default function Home() {
   return (
     <Wrapper>
       <Background />
       <Container>
-        <ContentWrapper>Home page</ContentWrapper>
+        <ContentWrapper>
+          <div>
+            <Title>Bounties</Title>
+          </div>
+
+          <div>
+            <SubTitle>Child Bounties</SubTitle>
+
+            <ChildBountyList></ChildBountyList>
+          </div>
+        </ContentWrapper>
       </Container>
     </Wrapper>
   );
