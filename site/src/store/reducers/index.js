6312f22d04095740e0701544fd7a52182868260e
2nthony <ant.hu@outlook.com>
2022-07-13 14:21:23 +0800

    feat: bounty slice

--- site/src/store/reducers/index.js
+++ site/src/store/reducers/index.js
@@ -4,10 +4,12 @@ import accountReducer from "./accountSlice";
 import nodeReducer from "./nodeSlice";
 import toastReducer from "./toastSlice";
 import showConnectReducer from "./showConnectSlice";
+import bountyReducer from "./bountySlice";
 
 export default combineReducers({
   account: accountReducer,
   node: nodeReducer,
   toast: toastReducer,
   showConnect: showConnectReducer,
+  bounty: bountyReducer,
 });
