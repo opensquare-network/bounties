7fbbcc370f716a21d9c0c0a4e6edf51f982e1481
2nthony <ant.hu@outlook.com>
2022-07-14 17:23:39 +0800

    fix: loading elements

--- site/src/components/Bounty/styled/metaLoading.js
+++ site/src/components/Bounty/styled/metaLoading.js
@@ -1,131 +1,15 @@
 import { Loading } from "./loading";
+import { ReactComponent as MetaLoading } from "./loading/meta/metaLoading.svg";
+import { ReactComponent as DescriptionLoading } from "./loading/meta/descriptionLoading.svg";
 
 export const metaLoading = (
   <Loading>
-    <svg
-      width="1016"
-      height="144"
-      viewBox="0 0 1016 144"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M0 32C0 14.3269 14.3269 0 32 0V0C49.6731 0 64 14.3269 64 32V32C64 49.6731 49.6731 64 32 64V64C14.3269 64 0 49.6731 0 32V32Z"
-        fill="url(#paint0_linear_8464_32463)"
-      />
-      <rect
-        y="88"
-        width="160"
-        height="16"
-        fill="url(#paint1_linear_8464_32463)"
-      />
-      <rect
-        y="128"
-        width="1016"
-        height="16"
-        fill="url(#paint2_linear_8464_32463)"
-      />
-      <defs>
-        <linearGradient
-          id="paint0_linear_8464_32463"
-          x1="0"
-          y1="32"
-          x2="64"
-          y2="32"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="#EFF3F9" />
-          <stop offset="1" stop-color="#F8FAFC" />
-        </linearGradient>
-        <linearGradient
-          id="paint1_linear_8464_32463"
-          x1="0"
-          y1="96"
-          x2="160"
-          y2="96"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="#EFF3F9" />
-          <stop offset="1" stop-color="#F8FAFC" />
-        </linearGradient>
-        <linearGradient
-          id="paint2_linear_8464_32463"
-          x1="0"
-          y1="136"
-          x2="1016"
-          y2="136"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="#EFF3F9" />
-          <stop offset="1" stop-color="#F8FAFC" />
-        </linearGradient>
-      </defs>
-    </svg>
+    <MetaLoading />
   </Loading>
 );
 
 export const descriptionLoading = (
   <Loading>
-    <svg
-      width="1016"
-      height="104"
-      viewBox="0 0 1016 104"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect
-        y="4"
-        width="1016"
-        height="16"
-        fill="url(#paint0_linear_9495_44204)"
-      />
-      <rect
-        y="44"
-        width="1016"
-        height="16"
-        fill="url(#paint1_linear_9495_44204)"
-      />
-      <rect
-        y="84"
-        width="160"
-        height="16"
-        fill="url(#paint2_linear_9495_44204)"
-      />
-      <defs>
-        <linearGradient
-          id="paint0_linear_9495_44204"
-          x1="0"
-          y1="12"
-          x2="1016"
-          y2="12.0003"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="#EFF3F9" />
-          <stop offset="1" stop-color="#F8FAFC" />
-        </linearGradient>
-        <linearGradient
-          id="paint1_linear_9495_44204"
-          x1="0"
-          y1="52"
-          x2="1016"
-          y2="52.0003"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="#EFF3F9" />
-          <stop offset="1" stop-color="#F8FAFC" />
-        </linearGradient>
-        <linearGradient
-          id="paint2_linear_9495_44204"
-          x1="0"
-          y1="92"
-          x2="160"
-          y2="92"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stop-color="#EFF3F9" />
-          <stop offset="1" stop-color="#F8FAFC" />
-        </linearGradient>
-      </defs>
-    </svg>
+    <DescriptionLoading />
   </Loading>
 );
