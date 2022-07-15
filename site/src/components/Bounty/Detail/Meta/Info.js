78af58c8a555f374a0e30b438fbd6954f18406c8
hyifeng <chaojun@opensquare.network>
2022-07-14 22:15:04 -0500

    Fix: bounty related links (#33)

--- site/src/components/Bounty/Detail/Meta/Info.js
+++ site/src/components/Bounty/Detail/Meta/Info.js
@@ -4,6 +4,9 @@ import NetworkUser from "components/User/NetworkUser";
 import ChainIcon from "@osn/common-ui/es/Chain/ChainIcon";
 import FlexCenter from "@osn/common-ui/es/styled/FlexCenter";
 import Tooltip from "@osn/common-ui/es/Tooltip";
+import { ReactComponent as PolkassemblyIcon } from "imgs/icons/polkassembly.svg";
+import { ReactComponent as SubSquareIcon } from "imgs/icons/subsquare.svg";
+import { ReactComponent as DotreasuryIcon } from "imgs/icons/dotreasury.svg";
 
 const Wrapper = styled.div`
   display: flex;
@@ -36,9 +39,14 @@ const InfoContent = styled.div`
   line-height: 24px;
   color: #1E2134;
   gap: 8px;
+  > a {
+    cursor: pointer;
+  }
 `;
 
 export default function Info({ bountyDetail }) {
+  const dotreasuryNetwork = bountyDetail?.network === "kusama" ? "ksm" : "dot";
+
   return (
     <Wrapper>
       <InfoItem
@@ -90,7 +98,18 @@ export default function Info({ bountyDetail }) {
       />
       <InfoItem
         title={<InfoHeader>Related Links</InfoHeader>}
-        content={<InfoContent>{"-"}</InfoContent>}
+        content={
+          <InfoContent>
+            <a target="_blank" rel="noreferrer" href={`https://${bountyDetail?.network}.subsquare.io/treasury/bounty/${bountyDetail?.bountyIndex}`}>
+              <SubSquareIcon />
+            </a>
+            <a target="_blank" rel="noreferrer" href={`https://dotreasury.com/${dotreasuryNetwork}/bounties/${bountyDetail?.bountyIndex}`}>
+              <DotreasuryIcon />
+            </a>
+            <a target="_blank" rel="noreferrer" href={`https://${bountyDetail?.network}.polkassembly.io/bounty/${bountyDetail?.bountyIndex}`}>
+              <PolkassemblyIcon />
+            </a>
+          </InfoContent>}
       />
     </Wrapper>
   );
