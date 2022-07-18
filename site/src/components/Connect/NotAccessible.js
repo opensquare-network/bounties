bcf591082537db0a20d75467c3ba27f88febb145
Yongfeng LI <wliyongfeng@gmail.com>
2022-07-18 10:19:16 +0800

    fix: connect modal flash (#50)
    * Refactor ConnectModal
    
    * Add line break
    
    * Remove console.log

---
+++ site/src/components/Connect/NotAccessible.js
@@ -0,0 +1,28 @@
+import { ActionBar, StyledDescription } from "./styled";
+import Button from "@osn/common-ui/es/styled/Button";
+import { Fragment, memo } from "react";
+import { useDispatch } from "react-redux";
+import { closeConnect } from "../../store/reducers/showConnectSlice";
+
+function NotAccessible() {
+  const dispatch = useDispatch();
+  const closeModal = () => dispatch(closeConnect());
+
+  return (
+    <Fragment>
+      <StyledDescription>
+        Polkadot-js extension is detected but unaccessible, please go to
+        Polkadot-js extension, settings, and check Manage Website Access
+        section.
+      </StyledDescription>
+
+      <ActionBar>
+        <Button color="orange" onClick={ closeModal }>
+          Got it.
+        </Button>
+      </ActionBar>
+    </Fragment>
+  );
+}
+
+export default memo(NotAccessible);
