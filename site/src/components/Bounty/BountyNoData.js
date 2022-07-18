a3d2057360b26716f174cd0321e9b26f2b159735
2nthony <ant.hu@outlook.com>
2022-07-18 12:17:52 +0800

    fix: no data bg (#55)
    * fix: img alt
    
    * bounty styled title css
    
    * fix: page home no data css

--- site/src/components/Bounty/BountyNoData.js
+++ site/src/components/Bounty/BountyNoData.js
@@ -3,7 +3,7 @@ import { Title, SubTitle, Head } from "./styled/bounty";
 export default function BountyNoData() {
   return (
     <Head>
-      <img src="/imgs/icons/no-bounties.svg" />
+      <img src="/imgs/icons/no-bounties.svg" alt="" />
       <Title>No current bounties</Title>
       <SubTitle>Get started by importing a bounty</SubTitle>
     </Head>
