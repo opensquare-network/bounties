ef2c54ea9faebfa26caa4c259cbffdb732b11e96
2nthony <ant.hu@outlook.com>
2022-07-19 18:21:50 +0800

    clean

--- site/src/store/reducers/index.js
+++ site/src/store/reducers/index.js
@@ -3,7 +3,6 @@ import { combineReducers } from "@reduxjs/toolkit";
 import accountReducer from "./accountSlice";
 import nodeReducer from "./nodeSlice";
 import toastReducer from "./toastSlice";
-import showConnectReducer from "./showConnectSlice";
 import bountyReducer from "./bountySlice";
 import discussionReducer from "./discussionSlice";
 
@@ -11,7 +10,6 @@ export default combineReducers({
   account: accountReducer,
   node: nodeReducer,
   toast: toastReducer,
-  showConnect: showConnectReducer,
   bounty: bountyReducer,
   discussion: discussionReducer,
 });
