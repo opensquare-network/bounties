ad033f8e14be9ba6388b6e9776bba7eda51b5e4f
2nthony <ant.hu@outlook.com>
2022-07-20 18:28:38 +0800

    feat: page notification

---
+++ site/src/pages/Notifications.js
@@ -0,0 +1,102 @@
+// Copied from qa
+
+import { useState } from "react";
+import styled from "styled-components";
+
+import { Pagination, Container, List, Flex } from "@osn/common-ui";
+import { useDispatch, useSelector } from "react-redux";
+import { clearUnread } from "store/reducers/notificationSlice";
+import { accountSelector } from "store/reducers/accountSlice";
+import NotificationItem from "../components/Notification/NotificationItem";
+import NotificationTabs from "../components/Notification/NotificationTabs";
+import { ReactComponent as CheckUnderline } from "@osn/common-ui/es/imgs/icons/check-underline.svg";
+import { text_dark_minor } from "@osn/common-ui/es/styles/colors";
+import { p_14_medium } from "@osn/common-ui/es/styles/textStyles";
+import { useNotifications } from "utils/useNotifications";
+
+const Wrapper = styled.div`
+  position: relative;
+  padding: 0 0 64px;
+`;
+
+const ContentWrapper = styled.div`
+  position: relative;
+  margin: 20px 0;
+`;
+
+const ReadAllButton = styled(Flex)`
+  color: ${text_dark_minor};
+  ${p_14_medium};
+  cursor: pointer;
+`;
+
+export default function Notifications() {
+  const dispatch = useDispatch();
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+  const account = useSelector(accountSelector);
+  const [tab, setTab] = useState("notifications");
+  const [isLoading, notifications, refresh] = useNotifications(
+    page,
+    account,
+    tab,
+    setPage,
+  );
+
+  return (
+    <Wrapper>
+      <NotificationTabs
+        items={[{ value: "notifications", suffix: notifications?.total }]}
+        value={tab}
+        setValue={setTab}
+        extra={
+          notifications?.items?.length > 0 && (
+            <ReadAllButton
+              role="button"
+              onClick={() => {
+                dispatch(clearUnread(account.network, account.address));
+                // do refresh
+                refresh();
+              }}
+            >
+              <CheckUnderline style={{ marginRight: 11 }} />
+              Mark all as read
+            </ReadAllButton>
+          )
+        }
+      />
+
+      <Container>
+        <ContentWrapper>
+          <List
+            gap={20}
+            loading={isLoading}
+            data={notifications?.items}
+            noDataMessage="No notifications"
+            itemKey={(item) => `${item._id}_${item.read}`}
+            itemRender={(item) => (
+              <List.Item>
+                <NotificationItem
+                  data={item}
+                  onMarkAsRead={(data) => {
+                    dispatch(
+                      clearUnread(account.network, account.address, {
+                        items: [data._id],
+                      }),
+                    );
+                  }}
+                />
+              </List.Item>
+            )}
+          />
+        </ContentWrapper>
+
+        <Pagination
+          {...{ page, setPage, pageSize }}
+          onChange={() => window.scrollTo(0, 0)}
+          total={notifications?.total}
+        />
+      </Container>
+    </Wrapper>
+  );
+}
---
+++ site/src/utils/useNotifications.js
@@ -0,0 +1,67 @@
+// copied from qa
+
+import { useCallback, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import serverApi from "../services/serverApi";
+import { EmptyList } from "./constants";
+import { unreadSelector } from "store/reducers/notificationSlice";
+
+export function useNotifications(page, account, tab, setPage) {
+  const pageSize = 10;
+  const [notifications, setNotifications] = useState(EmptyList);
+  const [isLoading, setIsLoading] = useState(true);
+  const unread = useSelector(unreadSelector);
+  const [prevUnread, setPrevUnread] = useState(unread);
+
+  const refresh = useCallback(() => {
+    if (account?.network && account?.address) {
+      serverApi
+        .fetch(
+          `https://test-qa-api.opensquare.io/network/${account.network}/address/${account.address}/notifications`,
+          { page, pageSize },
+        )
+        .then(({ result }) => {
+          if (result) {
+            setNotifications(result);
+          }
+        });
+    }
+  }, [account?.network, account?.address, tab, page, pageSize]);
+
+  useEffect(() => {
+    // Got new notifications
+    if (unread > prevUnread) {
+      refresh();
+    }
+
+    setPrevUnread(unread);
+  }, [unread, prevUnread, refresh]);
+
+  useEffect(() => {
+    setNotifications(null);
+    setPage(1);
+  }, [setPage, tab]);
+
+  useEffect(() => {
+    if (account?.network && account?.address) {
+      setIsLoading(true);
+      serverApi
+        .fetch(
+          `https://test-qa-api.opensquare.io/network/${account.network}/address/${account.address}/notifications`,
+          { page, pageSize },
+        )
+        .then(({ result }) => {
+          if (result) {
+            setNotifications(result);
+          } else {
+            setNotifications(EmptyList);
+          }
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  }, [account?.network, account?.address, page, tab]);
+
+  return [isLoading, notifications, refresh];
+}
