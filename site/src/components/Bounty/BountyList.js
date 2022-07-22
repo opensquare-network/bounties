a0b5feefc6ce915783418c2efef13e6aea7edf01
2nthony <ant.hu@outlook.com>
2022-07-22 11:52:39 +0800

    fix: bounty list mobile (#75)

--- site/src/components/Bounty/BountyList.js
+++ site/src/components/Bounty/BountyList.js
@@ -1,31 +1,13 @@
 import BountyItem from "./BountyItem";
-import styled from "styled-components";
-import { MOBILE_SIZE } from "@osn/constants";
 import BountyNoData from "./BountyNoData";
-
-const Wrapper = styled.div`
-  --gap: 20px;
-  --cols: 3;
-  --gaps: calc(var(--gap) * calc(var(--cols) - 1));
-  gap: var(--gap);
-  flex-wrap: wrap;
-  display: grid;
-  grid-template-columns: repeat(
-    var(--cols),
-    calc((100% - var(--gaps)) / var(--cols))
-  );
-
-  @media screen and (max-width: ${MOBILE_SIZE}px) {
-    display: block;
-  }
-`;
+import { BountyWrapper, BountyItemWrapper } from "./styled/bounty";
 
 export default function BountyList({ items = [], isLoading = false }) {
   if (isLoading) {
     return (
-      <Wrapper>
+      <BountyWrapper>
         <BountyItem.Loading />
-      </Wrapper>
+      </BountyWrapper>
     );
   }
 
@@ -34,11 +16,12 @@ export default function BountyList({ items = [], isLoading = false }) {
   }
 
   return (
-    <Wrapper>
-      {/* TODO: how many in mobile size */}
+    <BountyWrapper>
       {items?.map((item, idx) => (
-        <BountyItem key={idx} {...item}></BountyItem>
+        <BountyItemWrapper key={idx}>
+          <BountyItem {...item}></BountyItem>
+        </BountyItemWrapper>
       ))}
-    </Wrapper>
+    </BountyWrapper>
   );
 }
