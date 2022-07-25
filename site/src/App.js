adbd4b358a279344c3b3b990363ed1c9e69edb46
2nthony <ant.hu@outlook.com>
2022-07-25 20:02:03 +0800

    child bounty detail page

--- site/src/App.js
+++ site/src/App.js
@@ -9,6 +9,7 @@ import Toast from "components/Toast";
 import Home from "pages/Home";
 import ImportBounty from "pages/ImportBounty";
 import BountyDetail from "pages/BountyDetail";
+import ChildBountyDetail from "pages/ChildBountyDetail";
 import FourOFour from "pages/404";
 import ScrollToTop from "./components/ScrollToTop";
 import Notifications from "pages/Notifications";
@@ -26,6 +27,10 @@ function App() {
             <Route
               path="/network/:network/bounty/:bountyId"
               element={<BountyDetail />}
+            />
+            <Route
+              path="/network/:network/bounty/:bountyId/child-bounty/:childBountyId"
+              element={<ChildBountyDetail />}
             />
             <Route path="/notifications" element={<Notifications />} />
             <Route path="*" element={<FourOFour />} />
