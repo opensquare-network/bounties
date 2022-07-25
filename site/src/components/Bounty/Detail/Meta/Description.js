beec2b614a549550eca6bc8f215e85b0ddcb6895
2nthony <ant.hu@outlook.com>
2022-07-25 15:24:44 +0800

    refactor: Collapse instead of accordion

--- site/src/components/Bounty/Detail/Meta/Description.js
+++ site/src/components/Bounty/Detail/Meta/Description.js
@@ -1,20 +1,10 @@
-import styled from "styled-components";
-import { p_16_semibold } from "@osn/common-ui/es/styles/textStyles";
 import { MarkdownPreviewer } from "@osn/previewer";
-import Accordion from "components/Accordion";
-
-const Wrapper = styled.div``;
-
-const Title = styled.span`
-  ${p_16_semibold}
-`;
+import { Collapse } from "@osn/common-ui";
 
 export default function Description({ bountyDetail }) {
   return (
-    <Wrapper>
-      <Accordion title={<Title>Description</Title>}>
-        <MarkdownPreviewer content={bountyDetail.content} />
-      </Accordion>
-    </Wrapper>
+    <Collapse title="Description" ghost>
+      <MarkdownPreviewer content={bountyDetail.content} />
+    </Collapse>
   );
 }
