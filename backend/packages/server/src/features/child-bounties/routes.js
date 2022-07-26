ea210ed9b2d9b412362d97563a1394d8c584669d
hyifeng <chaojun@opensquare.network>
2022-07-26 09:41:35 +0800

    Add child bounty API (#80)

---
+++ backend/packages/server/src/features/child-bounties/routes.js
@@ -0,0 +1,25 @@
+const Router = require("koa-router");
+const childBountyController = require("./child-bounty.controller.js");
+const requireSignature = require("../../middleware/require-signature");
+
+const router = new Router();
+
+router.get("/child-bounties", childBountyController.getChildBounties);
+
+router.post(
+  "/child-bounties",
+  requireSignature,
+  childBountyController.importChildBounty,
+);
+
+router.get(
+  "/network/:network/child-bounties/:parentBountyIndex(\\d+)_:index(\\d+)",
+  childBountyController.getChildBounty,
+);
+
+router.get(
+  "/network/:network/child-bounties/:parentBountyIndex(\\d+)_:index(\\d+)/comments",
+  childBountyController.getChildBountyComments,
+);
+
+module.exports = router;
