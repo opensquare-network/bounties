7965cc6fe9482ed65142cb694f44fc0fbfa10f65
2nthony <ant.hu@outlook.com>
2022-07-28 14:57:54 +0800

    fix: bounty detail child bounties (#95)
    * fix: child bounty count
    
    * fix: bounty detail child bounties
    
    * refactor: resolve import child bounty route

--- site/src/components/Bounty/Detail/ChildBounties/index.js
+++ site/src/components/Bounty/Detail/ChildBounties/index.js
@@ -1,41 +1,80 @@
-import { useAsyncState } from "@osn/common";
-import { Collapse, List, Button } from "@osn/common-ui";
-import DetailLoader from "@osn/common-ui/es/Skeleton/DetailLoader";
-import { useCallback } from "react";
-import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import { Collapse, List, Button, Time, FlexCenter } from "@osn/common-ui";
+import { ReactComponent as Loading } from "imgs/icons/loading.svg";
+import BountyTag from "components/Bounty/BountyTag";
+import StatusLabel from "components/Bounty/StatusLabel";
+import {
+  resolveChildBountyDetailRoute,
+  resolveImportChildBountyRoute,
+} from "utils/route";
+import { Index, Item, ListWrapper, Status, TimeWrapper, Title } from "./styled";
 
 export default function ChildBounties({ bountyDetail = {} }) {
-  const navigate = useNavigate();
-
-  const goImportChild = useCallback(() => {
-    navigate(
-      `/import_child_bounty?network=${bountyDetail?.network}&parentBountyId=${bountyDetail?.bountyIndex}`,
-    );
-  }, [navigate, bountyDetail?.bountyIndex, bountyDetail?.network]);
-
-  const { state, isLoading } = useAsyncState(
-    () => [],
-    {},
-    {
-      onError: console.error,
-    },
-  );
+  const { childBounties = [], network, bountyIndex } = bountyDetail ?? {};
 
   return (
     <Collapse title="Child Bounties">
-      <List
-        data={state}
-        gap={20}
-        noDataMessage="No current bounties"
-        noDataProps={{ bordered: false, shadow: false }}
-        isLoading={isLoading}
-        loadingComponent={<DetailLoader />}
-        renderItem={(item) => <List.Item></List.Item>}
-      />
+      <ListWrapper>
+        <List
+          data={childBounties}
+          noDataMessage="No current bounties"
+          noDataProps={{ bordered: false, shadow: false }}
+          loading={!childBounties.length}
+          loadingComponent={
+            <FlexCenter>
+              <Loading />
+            </FlexCenter>
+          }
+          itemRender={(item, idx) => {
+            const i = idx + 1;
+            const {
+              parentBountyIndex,
+              network,
+              index: childBountyIndex,
+            } = item ?? {};
 
-      <Button block onClick={goImportChild}>
-        Import a Child Bounty
-      </Button>
+            return (
+              <List.Item>
+                <Item>
+                  <Index>#{i}</Index>
+
+                  <Status>
+                    <div>
+                      <StatusLabel>{item.status}</StatusLabel>
+                    </div>
+                    {item.status !== "open" && (
+                      <TimeWrapper>
+                        <Time time={item.createdAt} />
+                      </TimeWrapper>
+                    )}
+                  </Status>
+
+                  <Title>
+                    <Link
+                      to={resolveChildBountyDetailRoute(
+                        network,
+                        parentBountyIndex,
+                        childBountyIndex,
+                      )}
+                    >
+                      {item.title}
+                    </Link>
+                  </Title>
+
+                  <BountyTag {...item.childBounty} />
+                </Item>
+              </List.Item>
+            );
+          }}
+        />
+      </ListWrapper>
+
+      <Link
+        style={{ display: "flex" }}
+        to={resolveImportChildBountyRoute(network, bountyIndex)}
+      >
+        <Button block>Import a Child Bounty</Button>
+      </Link>
     </Collapse>
   );
 }
