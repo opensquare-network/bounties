3f181ed11b36c84f74e32130c8e9c744538db3e0
2nthony <ant.hu@outlook.com>
2022-08-03 16:42:14 +0800

    submitted action

--- site/src/components/ChildBounty/Detail/Meta/actions/index.js
+++ site/src/components/ChildBounty/Detail/Meta/actions/index.js
@@ -1,11 +1,13 @@
 import { CHILD_BOUNTY_STATUS } from "utils/constants";
 import { useApplyAction } from "./apply";
 import { useSubmitAction } from "./submit";
+import { useSubmitedAction } from "./submitted";
 
 export function useAction(childBountyDetail) {
   const { status } = childBountyDetail ?? {};
   const applyAction = useApplyAction();
   const submitAction = useSubmitAction();
+  const submittedAction = useSubmitedAction();
 
   // curator view
   // TODO: curator action
@@ -17,6 +19,8 @@ export function useAction(childBountyDetail) {
     return;
   } else if (status === CHILD_BOUNTY_STATUS.Started) {
     return submitAction;
+  } else if (status === CHILD_BOUNTY_STATUS.Submitted) {
+    return submittedAction;
   }
 
   // WorkDone
---
+++ site/src/components/ChildBounty/Detail/Meta/actions/submitted/index.js
@@ -0,0 +1,36 @@
+import { ButtonGroup, ButtonText } from "../styled";
+import { Button, Flex, Dot, FlexCenter, Time } from "@osn/common-ui";
+import { useSubmitModal } from "../submit/useSubmitModal";
+
+// FIXME: submitted action has default content and link
+export function useSubmitedAction() {
+  const { toggle, modal } = useSubmitModal({
+    // FIXME: submitted action submit
+    onConfirm(v) {
+      const { content, link } = v;
+    },
+  });
+
+  // FIXME: submit action cancel
+  function handleCancel() {}
+
+  return (
+    <>
+      <ButtonGroup>
+        <Flex>
+          <Button block primary disabled>
+            <FlexCenter>
+              <ButtonText>Submitted</ButtonText>
+              <Dot /> <Time time={new Date()} />
+            </FlexCenter>
+          </Button>
+
+          <Button onClick={toggle}>Update</Button>
+          <Button onClick={handleCancel}>Cancel</Button>
+        </Flex>
+      </ButtonGroup>
+
+      {modal}
+    </>
+  );
+}
