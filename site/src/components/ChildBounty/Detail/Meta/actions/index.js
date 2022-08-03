737d6730d1499dafc471fb15a71e4b9fda880ac4
2nthony <ant.hu@outlook.com>
2022-08-03 16:54:23 +0800

    curators view

--- site/src/components/ChildBounty/Detail/Meta/actions/index.js
+++ site/src/components/ChildBounty/Detail/Meta/actions/index.js
@@ -1,26 +1,39 @@
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { accountSelector } from "store/reducers/accountSlice";
 import { CHILD_BOUNTY_STATUS } from "utils/constants";
 import { useApplyAction } from "./apply";
 import { useSubmitAction } from "./submit";
 import { useSubmitedAction } from "./submitted";
 
 export function useAction(childBountyDetail) {
-  const { status } = childBountyDetail ?? {};
+  const { status, childBounty } = childBountyDetail ?? {};
+  const { curators = [] } = childBounty ?? {};
+  const account = useSelector(accountSelector);
+
+  const isCurator = useMemo(
+    () => curators.some((i) => i === account?.address),
+    [account?.address, curators],
+  );
+
   const applyAction = useApplyAction();
   const submitAction = useSubmitAction();
   const submittedAction = useSubmitedAction();
 
   // curator view
-  // TODO: curator action
-
+  if (isCurator) {
+  }
   // hunter view
-  if (status === CHILD_BOUNTY_STATUS.Open) {
-    return applyAction;
-  } else if (status === CHILD_BOUNTY_STATUS.Apply) {
-    return;
-  } else if (status === CHILD_BOUNTY_STATUS.Started) {
-    return submitAction;
-  } else if (status === CHILD_BOUNTY_STATUS.Submitted) {
-    return submittedAction;
+  else {
+    if (status === CHILD_BOUNTY_STATUS.Open) {
+      return applyAction;
+    } else if (status === CHILD_BOUNTY_STATUS.Apply) {
+      return;
+    } else if (status === CHILD_BOUNTY_STATUS.Started) {
+      return submitAction;
+    } else if (status === CHILD_BOUNTY_STATUS.Submitted) {
+      return submittedAction;
+    }
   }
 
   // WorkDone
