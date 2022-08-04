5d1f53c4616c5a9a431c80258fdf31c9f881bbc0
2nthony <ant.hu@outlook.com>
2022-08-04 15:55:22 +0800

    fix: signed data

--- site/src/hooks/useWorkflowActionService.js
+++ site/src/hooks/useWorkflowActionService.js
@@ -1,27 +1,26 @@
 import { useAccount } from "hooks/useAccount";
 import serverApi from "services/serverApi";
+import { signApiData } from "utils/signature";
 
 export function useWorkflowActionService(childBountyDetail) {
   const { network, parentBountyIndex, index } = childBountyDetail ?? {};
   const account = useAccount();
 
   const data = {
-    address: account?.address,
-    signature: "test-signature",
-    data: {
-      network,
-      parentBountyIndex,
-      index,
-    },
+    action: "",
+    network,
+    parentBountyIndex,
+    index,
   };
 
   async function applyService(value) {
-    data.data.action = "applyChildBounty";
-    data.data.description = value.content;
-    data.data.applicantNetwork = account?.network;
+    data.action = "applyChildBounty";
+    data.description = value.content;
+    data.applicantNetwork = account?.network;
+    const signedData = await signApiData(data, account?.address);
 
     try {
-      const res = await serverApi.post("/applications", data);
+      const res = await serverApi.post("/applications", signedData);
 
       // TODO: error toast
       if (res.error) {
