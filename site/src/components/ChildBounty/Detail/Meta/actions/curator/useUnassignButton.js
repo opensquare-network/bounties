2ef084be3508c489a0d4ac7edb125fd8c25a25fe
2nthony <ant.hu@outlook.com>
2022-08-04 20:53:18 +0800

    unassign button

---
+++ site/src/components/ChildBounty/Detail/Meta/actions/curator/useUnassignButton.js
@@ -0,0 +1,12 @@
+// TODO: implement
+import { Button } from "@osn/common-ui";
+
+export function useCuratorUnassignButton(childBountyDetail) {
+  const {} = childBountyDetail ?? {};
+
+  const unassignButton = <Button>Unassign</Button>;
+
+  return {
+    unassignButton,
+  };
+}
