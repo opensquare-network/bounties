8e955673628429a4c77c52fd28d09dd80fd40bf9
2nthony <ant.hu@outlook.com>
2022-08-04 17:48:03 +0800

    status label add more status color

--- site/src/components/Bounty/StatusLabel/index.js
+++ site/src/components/Bounty/StatusLabel/index.js
@@ -1,17 +1,18 @@
 import {
   primary_purple_500,
   secondary_green_500,
-  secondary_red_500,
   text_dark_accessory,
-} from "@osn/common-ui/es/styles/colors";
+} from "@osn/common-ui";
+import { APPLICATION_STATUS, CHILD_BOUNTY_STATUS } from "utils/constants";
 import { Wrapper } from "./styled";
 
 const COLOR_MAP = {
-  open: secondary_green_500,
-  closed: secondary_red_500,
-  started: primary_purple_500,
-  submitted: primary_purple_500,
-  "work done": text_dark_accessory,
+  [CHILD_BOUNTY_STATUS.Open]: secondary_green_500,
+  [CHILD_BOUNTY_STATUS.Started]: primary_purple_500,
+  [CHILD_BOUNTY_STATUS.Submitted]: primary_purple_500,
+  [CHILD_BOUNTY_STATUS.Assigned]: primary_purple_500,
+  [CHILD_BOUNTY_STATUS.WorkDone]: text_dark_accessory,
+  [APPLICATION_STATUS.Cancelled]: text_dark_accessory,
 };
 
 /**
