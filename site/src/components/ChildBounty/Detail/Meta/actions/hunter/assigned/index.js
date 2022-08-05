8b65feab0b508d4d4c0fdf66b544dada48c5cdb7
2nthony <ant.hu@outlook.com>
2022-08-05 17:33:50 +0800

    hunter assigned

--- site/src/components/ChildBounty/Detail/Meta/actions/hunter/assigned/index.js
+++ site/src/components/ChildBounty/Detail/Meta/actions/hunter/assigned/index.js
@@ -1,21 +1,46 @@
-import { Flex } from "@osn/common-ui";
+import { Flex, Button } from "@osn/common-ui";
 import { useAccount } from "hooks/useAccount";
+import { useWorkflowActionService } from "hooks/useWorkflowActionService";
 import AssignedToButton from "../../components/AssignedToButton";
 import { ButtonGroup } from "../../styled";
 import { findAssignedApplicant } from "../../utils";
+import { useHunterCancelButton } from "../useCancelButton";
 
 export function useHunterAssignedAction(childBountyDetail) {
   const { applications = [] } = childBountyDetail ?? {};
   const account = useAccount();
+  const { cancelButton } = useHunterCancelButton();
+  const { acceptService } = useWorkflowActionService(childBountyDetail);
 
   const assignedApplicant = findAssignedApplicant(applications);
 
-  const isAssignedToMe = account?.address === assignedApplicant?.address;
+  const isAssignedToMe = account?.encodedAddress === assignedApplicant?.address;
+
+  function handleAcceptAndStart() {
+    acceptService({
+      applicantAddress: assignedApplicant.address,
+      applicantNetwork: assignedApplicant.bountyIndexer.network,
+    });
+  }
+
+  const acceptAndStartEl = (
+    <>
+      <Button primary block onClick={handleAcceptAndStart}>
+        Accept and Start
+      </Button>
+
+      {cancelButton}
+    </>
+  );
 
   return (
     <ButtonGroup>
       <Flex>
-        <AssignedToButton assignedApplicant={assignedApplicant} />
+        {isAssignedToMe ? (
+          acceptAndStartEl
+        ) : (
+          <AssignedToButton assignedApplicant={assignedApplicant} />
+        )}
       </Flex>
     </ButtonGroup>
   );
