81d178ca3ef0f857029304c8b2240e4b1aac81b0
2nthony <ant.hu@outlook.com>
2022-08-08 15:52:48 +0800

    fix: hunter open safe read appliedApplicant

--- site/src/components/ChildBounty/Detail/Meta/actions/hunter/open/index.js
+++ site/src/components/ChildBounty/Detail/Meta/actions/hunter/open/index.js
@@ -73,7 +73,7 @@ export function useHunterOpenAction(childBountyDetail, reloadData) {
           <FlexCenter>
             <ButtonText>Applied</ButtonText>
             <Dot />
-            <Time time={appliedApplicant.updatedAt} />
+            <Time time={appliedApplicant?.updatedAt} />
           </FlexCenter>
         </Button>
 
