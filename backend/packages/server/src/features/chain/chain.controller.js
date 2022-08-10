9c6f8858837124c2afba41d112a052bad6106242
chaojun <chaojun@opensquare.network>
2022-08-10 12:38:36 +0800

    Fix: get onchain bounty info

--- backend/packages/server/src/features/chain/chain.controller.js
+++ backend/packages/server/src/features/chain/chain.controller.js
@@ -1,17 +1,30 @@
 const chainService = require("../../services/chain.service");
+const { HttpError } = require("../../utils/exc");
 
 async function getBounty(ctx) {
   const { network, bountyIndex } = ctx.params;
-  ctx.body = await chainService.getBounty(network, bountyIndex);
+  const bounty = await chainService.getBounty(network, bountyIndex);
+
+  if (!bounty) {
+    throw new HttpError(404, "Bounty is not found on chain");
+  }
+
+  ctx.body = bounty;
 }
 
 async function getChildBounty(ctx) {
   const { network, parentBountyIndex, index } = ctx.params;
-  ctx.body = await chainService.getChildBounty(
+  const childBounty = await chainService.getChildBounty(
     network,
     parentBountyIndex,
     index,
   );
+
+  if (!childBounty) {
+    throw new HttpError(404, "Child bounty is not found on chain");
+  }
+
+  ctx.body = childBounty;
 }
 
 module.exports = {
