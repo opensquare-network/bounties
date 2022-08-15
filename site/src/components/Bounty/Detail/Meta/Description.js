df1b11e6c10a59853e1df2e13b13e0a580c5fc2f
2nthony <ant.hu@outlook.com>
2022-08-15 12:36:16 +0800

    can edit bounty

--- site/src/components/Bounty/Detail/Meta/Description.js
+++ site/src/components/Bounty/Detail/Meta/Description.js
@@ -1,22 +1,15 @@
 import { MarkdownPreviewer } from "@osn/previewer";
 import { Collapse } from "@osn/common-ui";
 import More from "components/Common/Detail/More";
-import { useDifferentNetworkNotice } from "hooks/useDifferentNetworkNotice";
-import { useIsCurator } from "hooks/useIsCurator";
+import { usePermission } from "hooks/usePermission";
 
 export default function Description({ bountyDetail, onEdit }) {
-  const { isSameNetwork } = useDifferentNetworkNotice(bountyDetail?.network);
-  const isCurator = useIsCurator(
-    // for bounty
-    bountyDetail?.bounty?.curators,
-    // for child bounty
-    bountyDetail?.childBounty?.curators,
-  );
+  const { canEditBounty } = usePermission(bountyDetail);
 
   return (
     <Collapse title="Description" ghost>
       <MarkdownPreviewer content={bountyDetail.content} />
-      {isSameNetwork && isCurator && <More onEdit={onEdit} />}
+      {canEditBounty && <More onEdit={onEdit} />}
     </Collapse>
   );
 }
