28b8c20bc340efa7d94913e2c4ccaa1b11e99b94
2nthony <ant.hu@outlook.com>
2022-08-22 15:10:13 +0800

    fix: ellipsis

--- site/src/components/Ellipsis/styled.js
+++ site/src/components/Ellipsis/styled.js
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const EllipsisWrapper = styled.span`
-  text-overflow: ellipsis;
-  white-space: nowrap;
+  word-break: break-all;
+
+  display: -webkit-inline-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
   overflow: hidden;
 `;
