8aaa56d88ee32f22dbfe85c1b55b68eb22480d03
2nthony <ant.hu@outlook.com>
2022-08-22 16:42:15 +0800

    Fix/bounty detail child list (#194)
    * use is screen hook
    
    * fix: bounty detail child list

--- site/src/components/Bounty/Detail/ChildBounties/index.js
+++ site/src/components/Bounty/Detail/ChildBounties/index.js
@@ -6,6 +6,9 @@ import {
   Time,
   FlexCenter,
   LoadingIcon,
+  Dot,
+  Flex,
+  FlexBetween,
 } from "@osn/common-ui";
 import BountyTag from "components/Bounty/BountyTag";
 import StatusLabel from "components/Bounty/StatusLabel";
@@ -13,9 +16,19 @@ import {
   resolveChildBountyDetailRoute,
   resolveImportChildBountyRoute,
 } from "utils/route";
-import { Index, Item, ListWrapper, Status, TimeWrapper, Title } from "./styled";
+import {
+  Index,
+  IndexWrapper,
+  Item,
+  ListWrapper,
+  MobileTitleGap,
+  Status,
+  TimeWrapper,
+  Title,
+} from "./styled";
 import { BOUNTY_STATUS } from "utils/constants";
 import { useBountyPermission } from "hooks/useBountyPermission";
+import { useIsScreen } from "hooks/useIsScreen";
 
 export default function ChildBounties({ bountyDetail = {} }) {
   const {
@@ -25,6 +38,8 @@ export default function ChildBounties({ bountyDetail = {} }) {
     bountyIndex,
   } = bountyDetail ?? {};
   const { canImportChildBounty } = useBountyPermission(bountyDetail);
+
+  const { isDesktop } = useIsScreen();
 
   return (
     <Collapse title="Child Bounties">
@@ -41,42 +56,14 @@ export default function ChildBounties({ bountyDetail = {} }) {
           }
           itemRender={(item, idx) => {
             const i = idx + 1;
-            const {
-              parentBountyIndex,
-              network,
-              index: childBountyIndex,
-            } = item ?? {};
 
             return (
               <List.Item>
-                <Item>
-                  <Index>#{i}</Index>
-
-                  <Status>
-                    <div>
-                      <StatusLabel>{item.status}</StatusLabel>
-                    </div>
-                    {item.status !== "open" && (
-                      <TimeWrapper>
-                        <Time time={item.updatedAt} />
-                      </TimeWrapper>
-                    )}
-                  </Status>
-
-                  <Title>
-                    <Link
-                      to={resolveChildBountyDetailRoute(
-                        network,
-                        parentBountyIndex,
-                        childBountyIndex,
-                      )}
-                    >
-                      {item.title}
-                    </Link>
-                  </Title>
-
-                  <BountyTag {...item.childBounty} />
-                </Item>
+                {isDesktop ? (
+                  <DesktopListItem item={item} i={i} />
+                ) : (
+                  <MobileListItem item={item} i={i} />
+                )}
               </List.Item>
             );
           }}
@@ -92,5 +79,81 @@ export default function ChildBounties({ bountyDetail = {} }) {
         </Link>
       )}
     </Collapse>
+  );
+}
+
+function DesktopListItem({ item, i }) {
+  const { parentBountyIndex, network, index: childBountyIndex } = item ?? {};
+
+  return (
+    <Item>
+      <IndexWrapper>
+        <Index>#{i}</Index>
+      </IndexWrapper>
+
+      <Status>
+        <div>
+          <StatusLabel>{item.status}</StatusLabel>
+        </div>
+        {item.status !== "open" && (
+          <TimeWrapper>
+            <Time time={item.updatedAt} />
+          </TimeWrapper>
+        )}
+      </Status>
+
+      <Title>
+        <Link
+          to={resolveChildBountyDetailRoute(
+            network,
+            parentBountyIndex,
+            childBountyIndex,
+          )}
+        >
+          {item.title}
+        </Link>
+      </Title>
+
+      <BountyTag {...item.childBounty} />
+    </Item>
+  );
+}
+
+function MobileListItem({ item, i }) {
+  const { parentBountyIndex, network, index: childBountyIndex } = item ?? {};
+
+  return (
+    <>
+      <FlexBetween>
+        <Flex>
+          <Index>#{i}</Index>
+          <Dot />
+          <StatusLabel>{item.status}</StatusLabel>
+
+          {item.status !== "open" && (
+            <>
+              <Dot />
+              <Time time={item.updatedAt} />
+            </>
+          )}
+        </Flex>
+
+        <BountyTag {...item.childBounty} />
+      </FlexBetween>
+
+      <MobileTitleGap />
+
+      <Title>
+        <Link
+          to={resolveChildBountyDetailRoute(
+            network,
+            parentBountyIndex,
+            childBountyIndex,
+          )}
+        >
+          {item.title}
+        </Link>
+      </Title>
+    </>
   );
 }
