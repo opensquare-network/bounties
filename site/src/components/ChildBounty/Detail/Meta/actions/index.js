304500d31e426b781d952e72f00341132f415c71
2nthony <ant.hu@outlook.com>
2022-08-31 19:03:25 +0800

    clean outdated logic

--- site/src/components/ChildBounty/Detail/Meta/actions/index.js
+++ site/src/components/ChildBounty/Detail/Meta/actions/index.js
@@ -2,8 +2,6 @@ import { useAccount } from "hooks/useAccount";
 import { CHILD_BOUNTY_STATUS } from "utils/constants";
 import { useHunterOpenAction } from "./hunter/open";
 import { useCuratorOpenAction } from "./curator/open";
-import { useCuratorSubmittedAction } from "./curator/submitted";
-import { useCuratorWorkDoneAction } from "./curator/workDone";
 import { useCuratorAssignedAction } from "./curator/assigned";
 import { useHunterAssignedAction } from "./hunter/assigned";
 import { useCuratorClosedChildBountyAction } from "./curator/closed";
@@ -18,8 +16,6 @@ export function useAction(childBountyDetail) {
   const hunterOpenAction = useHunterOpenAction(childBountyDetail);
   const hunterAssignedAction = useHunterAssignedAction(childBountyDetail);
   const curatorOpenAction = useCuratorOpenAction(childBountyDetail);
-  const curatorSubmittedAction = useCuratorSubmittedAction();
-  const curatorWorkDoneAction = useCuratorWorkDoneAction();
   const curatorAssignedAction = useCuratorAssignedAction(childBountyDetail);
   const curatorClosedChildBountyAction =
     useCuratorClosedChildBountyAction(childBountyDetail);
@@ -30,10 +26,6 @@ export function useAction(childBountyDetail) {
       return curatorOpenAction;
     } else if (status === CHILD_BOUNTY_STATUS.Assigned) {
       return curatorAssignedAction;
-    } else if (status === CHILD_BOUNTY_STATUS.Started) {
-      return curatorSubmittedAction;
-    } else if (status === CHILD_BOUNTY_STATUS.WorkDone) {
-      return curatorWorkDoneAction;
     } else if (status === CHILD_BOUNTY_STATUS.Closed) {
       return curatorClosedChildBountyAction;
     }
@@ -47,6 +39,5 @@ export function useAction(childBountyDetail) {
     }
   }
 
-  // WorkDone
   return null;
 }
