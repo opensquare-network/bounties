832fa69095c1f79d4e3f260e158a645f68d53ab9
2nthony <ant.hu@outlook.com>
2022-10-28 18:00:01 +0800

    feat: form tip error color

--- site/src/components/ChildBounty/Detail/Meta/actions/styled.js
+++ site/src/components/ChildBounty/Detail/Meta/actions/styled.js
@@ -1,9 +1,10 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import {
   p_14_medium,
   p_14_normal,
   p_16_semibold,
   p_20_semibold,
+  secondary_red_500,
   text_dark_accessory,
   text_dark_major,
   text_dark_minor,
@@ -30,6 +31,12 @@ export const FormLabel = styled.h4`
 export const FormLabelTip = styled.span`
   ${p_14_normal};
   color: ${text_dark_accessory};
+
+  ${(p) =>
+    p.error &&
+    css`
+      color: ${secondary_red_500};
+    `}
 `;
 export const FormLabelWrapper = styled.div`
   margin-top: 20px;
