8c62789083e5c894a092377a3bc1f7d7a62f528a
2nthony <ant.hu@outlook.com>
2023-05-16 21:04:03 +0800

    fix polyfill warnings

--- site/vite.config.js
+++ site/vite.config.js
@@ -2,20 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import svgr from "vite-plugin-svgr";
-import nodePolyfills from "rollup-plugin-node-polyfills";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 const alias = (alias) => path.resolve(__dirname, alias);
 
 export default defineConfig({
-  plugins: [react(), svgr(), nodePolyfills()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
-      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
-      path: "rollup-plugin-node-polyfills/polyfills/path",
-      url: "rollup-plugin-node-polyfills/polyfills/url",
-
       "@": alias("src"),
       "@osn/common-ui/es": alias("node_modules/@osn/common-ui/dist/esm"),
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [NodeModulesPolyfillPlugin()],
     },
   },
 });
